091290572ba6ce6274f5b69ab0ccfc8f97691000
my first github repo <mrarifmondol98@gmail.com>
2026-08-26 18:44:34 +0600

    JS problem solving

--- problem-20.js
+++ problem-20.js
@@ -1,7 +1,10 @@
 // Both input arrays are already sorted ascending. Return one sorted array containing all their elements. Duplicates are kept.
 
 const mergeSorted = (a, b) => {
-    
+    const marge = [...a, ...b]
+    const sorted = marge.sort((a, b) => a - b)
+    return sorted
 }
 
 console.log(mergeSorted([1, 3, 5], [2, 4, 6]));
+console.log(mergeSorted([1, 5, 9], [2, 3]));
